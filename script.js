4818fa68251b26018b6fd4e2db6bf7fe3350e850
Axxo10 <182255376+Axxo10@users.noreply.github.com>
2026-02-10 17:48:02 -0600

    botones funcionales y resto

--- script.js
+++ script.js
@@ -1,71 +1,136 @@
-const tareaEntrada = document.getElementById('tareaEntrada');
-const botonAgregar = document.getElementById('botonAgregar');
+const tareaEntrada = document.getElementById("tareaEntrada");
+const botonAgregar = document.getElementById("botonAgregar");
+const contenedorTareas = document.getElementById("contenedorTareas");
+const mensaje = document.getElementById("mensaje");
+const contadorTotales = document.getElementById("contadorTotales");
+const contadorTerminadas = document.getElementById("contadorTerminadas");
+
 
 function crearElementoTarea(){
-    // Crear los elemntos html de la tarea
+
     const tareaContenedor = document.createElement('div');
     const tareaTexto = document.createElement('p');
     const iconoContenedor = document.createElement('div');
     const iconoCompletada = document.createElement('i');
     const iconoEliminar = document.createElement('i');
 
-    /* Creamos la estructura de la tarea */
 
     iconoContenedor.append(iconoCompletada , iconoEliminar);
     tareaContenedor.append(tareaTexto , iconoContenedor);
 
-    /* Agregamos las clases a los contenedores de la tarea */
 
     tareaContenedor.classList.add('tarea');
     tareaTexto.classList.add('tarea-texto');
     iconoContenedor.classList.add('tarea-iconos');
-    iconoCompletada.classList.add('bis' , 'bi-dash-circle');
+    iconoCompletada.classList.add('bis' , 'bi-check-circle');
     iconoEliminar.classList.add('bis' , 'bi-trash2');
 
     tareaTexto.innerText = tareaEntrada.value;
-
     iconoCompletada.addEventListener('click', (e) => {
-        const tareaElemento = e.target.parentNode.parentNode;
-        const esCompletada = tareaElemento.classList.contains('tarea-completada')
-        tareaElemento.classList.Toggle('tarea-completada');
-        if(esCompletada) {
 
+        const tareaElemento = e.target.parentNode.parentNode
+        const esCompletada = tareaElemento.classList.contains('tarea-completada')
+
+        tareaElemento.classList.toggle('tarea-completada');
+        if(esCompletada){
+            e.target.classList.remove('bi-dash-circle')
+            e.target.classList.add('bi-check-circle');
         }else{
-            
+            e.target.classList.add('bi-dash-circle');
+            e.target.classList.remove('bi-check-circle')
         }
+        actualizarContadores();
+
     })
 
+    iconoEliminar.addEventListener('click', (e) => {
+        const tareaElemento = e.target.parentNode.parentNode;
+        tareaElemento.remove();
+        actualizarContadores();
+    })
+
+    return tareaContenedor;
+
 }
-function agregarTarea(){
-    const texto = tareaEntrada.Value.trim()
-    if(texto) {
 
-    const elementoTarea = crearElementoTarea();
-    contenedorTarea.append(elementoTarea);
-    
-    tareaEntrada.Value = 'yuh';
 
-    mensaje.textcontent = 'Tarea'
+function actualizarContadores(){
+
+    const tareasTotales = document.querySelectorAll('.tarea');
+    const tareasCompletadas = document.querySelectorAll('.tarea-completada');
+
+   contadorTotales.textContent = tareasTotales.length;
+   contadorTerminadas.textContent = tareasCompletadas.length;
+}
+
+
+botonAgregar.addEventListener("click" , agregarTarea);
+
+
+    function agregarTarea(){
+
+        const texto = tareaEntrada.value.trim();
+
+        if(!texto) {
+            mensaje.textContent = 'Urhm faltatexto 🤓☝';
+        }else{
+             const elementoTarea = crearElementoTarea();
+             contenedorTareas.append(elementoTarea);
+
+             tareaEntrada.value = ' ';
+
+             mensaje.textContent = 'listop,ya está';
+
+             actualizarContadores();
+        }
 
     }
-   else{
-    mensaje.textcontent = 'escribe algo primero plis'
-   }
-}
+
+
+document.addEventListener('keydown', (e) => {
+    if(e.key == "Enter") {
+        agregarTarea();
+    }
+})
 
 tareaEntrada.addEventListener('input', () => {
-    if(tareaEntrada.Value.trim() === ""){
-        mensaje.textcontent = 'Pon algo'
+    if(tareaEntrada.value.trim() === ""){
+        mensaje.textContent = 'Otra????'
     }else{
-        mensaje.textcontent = 'enter'
+        mensaje.textContent = 'Enter o todavia falta?'
     }
 })
 
-botonAgregar.addEventListener('click',crearElementoTarea);
+let tareasOcultas = false;
 
 
-document.addEventListener('keydown', (e) => {    
-if(e.key == "Enter") {
- agregarTarea();
-} 
-})
+function toggleOcultarCompletadas(){
+
+  const tareasCompletadas = document.querySelectorAll(".tarea-completada");
+
+
+  
+  tareasCompletadas.forEach (  (tarea) => {
+    if(tareasOcultas)     
+    {
+      tarea.style.display = "flex";
+    }
+    
+    else{
+      tarea.style.display = "none";
+    }
+  }   );
+console.log(tareasOcultas);
+
+tareasOcultas = !tareasOcultas;
+
+console.log(tareasOcultas);
+}
+ toggleOcultarCompletadas()
+
+ function eliminarCompletadas () {
+    const tareasCompletadas = document.querySelectorAll('.tareas-completadas')
+    tareasCompletadas.forEach( (trea) => {tarea.remove()})
+    
+ }
+
